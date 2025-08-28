3eb77dcd496b897e9114213b07c1ab69f1c038bd
pdoupe <patrick.doupe@deliveryhero.com>
2025-08-28 16:05:16 +0200

    Draft results: opt for max roas

--- reco/higher_budget_lower_revenue.py
+++ reco/higher_budget_lower_revenue.py
@@ -15,6 +15,7 @@ def main():
     query = """
         SELECT * 
         FROM dhh-ncr-stg.performance_estimation.PEYA_cpc_budget_recos
+        WHERE model_name = 'cpc_budget_recos_v1.0_new'
     """
 
     df = pandas_gbq.read_gbq(query)
@@ -26,20 +27,19 @@ def main():
     df_2 = df.loc[df.reco_num == 3]
     tmp = pd.merge(df_1[cols], df_2[cols], how='inner', on=['global_entity_id', 'vendor_id', 'reco_date'], suffixes=['_1', '_2'])
     
-    tmp['lower_nr_1'] = tmp.net_revenue_1 < tmp.net_revenue_2
+    tmp['lower_nr_1'] = (tmp.net_revenue_1 < tmp.net_revenue_2) & (tmp.net_revenue_1 > 0)
     tmp['lower_gmv_1'] = tmp.e_cpc_gmv_1 < tmp.e_cpc_gmv_2
     tmp['lower_budget_1'] = tmp.reco_budget_lc_1 < tmp.reco_budget_lc_2
     tmp['lower_roas_1'] = tmp.e_roas_1 < tmp.e_roas_2
 
-    print(f"""The share of vendors that have a lower budget for reco 1:
-          {tmp.lower_budget_1.mean():.2f}""")
+    print(f"""The share of vendors (with recommendation > estimated gmv) that
+          have a lower budget for reco 1: {tmp.lower_budget_1.mean():.2f}""")
     print(f"""The share of vendors that have a lower NR for reco 1:
           {tmp.lower_nr_1.mean():.2f}""")
     print(f"""The share of vendors that have a lower GMV for reco 1:
           {tmp.lower_gmv_1.mean():.2f}""")
     print(f"""The share of vendors that have a lower ROAS for reco 1:
           {tmp.lower_roas_1.mean():.2f}""")
-
 
 if __name__=="__main__":
 
